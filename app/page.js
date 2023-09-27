34d14b755a77e6dfa6738d78757f2df975590100
Rahul <129066661+ranarahul23@users.noreply.github.com>
2023-09-27 21:00:04 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,38 @@
+"use client"
+import axios from "axios";
+import React, { useState } from "react";
+
+const page=()=>{
+  const [Images,setImages]=useState([]);
+  
+  const getImages=async ()=>{
+    try{
+      const response=await axios.get("https://picsum.photos/v2/list");
+      const data=response.data;
+      setImages(data);
+      console.log(Images);
+  
+    }catch(error){
+      console.error("error fetching")
+    }
+  };
+  return(
+    <div>
+      <button onClick={getImages}className="px-5 py-3 bg-green-800 text-white font-bold">
+        Get Images</button>
+      <div className="p-10">
+        {Images.map((elem,i)=>{
+          return <img 
+          key={i}
+          src={elem.download_url}
+          width={300}
+          height={300}
+          className="m-10 rounded inline-block"
+          />
+        })}
+      </div>
+    </div>
+
+  );
+};
+export default page
